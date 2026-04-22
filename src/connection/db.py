3d67f492ac3035b38bef349b74945853724f5418
Apeksha Malik <malikapeksha@gmail.com>
2026-04-22 11:41:55 -0400

    feat(connection): set up sqlite db

--- src/connection/db.py
+++ src/connection/db.py
@@ -1,15 +1,20 @@
 import sqlite3
 from pathlib import Path
+import os
+
+DB_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../database/mlb.db"))
+
+print(f"DB_PATH is: {DB_PATH}")  
 
 def get_connection():
-    conn = sqlite3.connect('mlb.db', check_same_thread=False)
+    conn = sqlite3.connect(DB_PATH, check_same_thread=False)
     conn.execute("PRAGMA foreign_keys = ON;")
     conn.row_factory = sqlite3.Row
     return conn
 
 
 def init_db():
-    schema_path = Path(__file__).parent / "schema.sql"
+    schema_path = Path("src/database/schema.sql")
     
     with get_connection() as conn:
         with open(schema_path) as f:
